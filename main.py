b72aae3b050db18a044a64e0a3ac866e8cfd16eb
SeclaPanda <61236755+SeclaPanda@users.noreply.github.com>
2020-12-10 01:03:26 +0300

    add new py-file
    add new py-file coz I completely recreate the app

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-import tkinter, time
 from tkinter import *
 from tkinter.ttk import *
 
-def clicked():
-    #lbl.configure(text = 'Loading, please wait')
-    #inp = login.get() + '\n' + passw.get() + '\n\n'
-    #with open('users.txt', 'a+') as f:
-    #    f.write(inp)
-    #lbl.configure(text = 'Input login and password')
-    global window
-    window.destroy()
-    import user
 def exit():
     global window
     window.destroy()
+
+def clicked():
+    '''
+    search = login.get() + ':' + passw.get()
+    index = 0
+    if search != ' ':
+        with open('C:\pyton\курсач\data.txt', 'r') as data:
+            for index, line in enumerate(data):
+                if search in line:
+                    if index == 0:
+                    #exit()
+                        import user
+    '''
+    global window
+    window.destroy()
+    
+    import user
 
 #create window
 window = Tk()
 window.title('Bank app v0.1')
 window.geometry('500x500')
 
+log = StringVar()
+pas = StringVar()
+
 lbl = Label(window, text = 'Input login and password', font = ('Arial Bold', 30))
 lbl.grid(column = 0, row = 0) #position
 
-login = Entry(window, width=10)
+login = Entry(window, width=10, textvariable=log)
 login.grid(column = 0, row = 1)
 login.focus()
-passw = Entry(window, width=10)
+passw = Entry(window, width=10, textvariable=pas)
 passw.grid(column = 0, row = 2)
 
-btn = Button(window, text = 'login', command = clicked)
-btn.grid(column = 0, row = 3)
+login_btn = Button(window, text = 'login', command = clicked())
+login_btn.grid(column = 0, row = 3)
 
-exit = Button(window, text = 'exit', command = exit)
-exit.grid(column = 1, row = 3)
+exit_btn = Button(window, text = 'exit', command = exit)
+exit_btn.grid(column = 1, row = 3)
 
 #loop for show window
 window.mainloop()
--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+'''
 import tkinter, time
 from tkinter import *
 from tkinter.ttk import *
@@ -8,3 +9,23 @@ user.geometry('500x500')
 lbl = Label(user, text = 'Input login and password or register', font = ('Arial Bold', 20))
 lbl.grid(column = 0, row = 0)
 user.mainloop()
+'''
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import *
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.lbl = Label(self, text = 'Input login and password or register', font = ('Arial Bold', 20))
+        self.lbl.grid(column = 0, row = 0)
+
+    def say_hello(self):
+        messagebox.showinfo("GUI Python", 'Hello, world')
+
+
+if __name__ == "__main__":
+    app = App()
+    app.title('Bank app for user v0.1')
+    app.geometry('500x500')
+    app.mainloop()
